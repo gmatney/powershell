f568711af7a57ceb504eaca3d70d0c05fc810c27
gmatney <gmatney@users.noreply.github.com>
2016-01-11 22:42:56 -0500

    batter function - If battery isn't found, print a message.  (It's good to take it out when sitting for a while)

--- machineDetails.ps1
+++ machineDetails.ps1
@@ -3,4 +3,7 @@
 function battery{
   $battery=(Get-WmiObject win32_battery)
   $battery|select DeviceID,Description,EstimatedChargeRemaining,EstimatedRunTime|fl
+  if($battery -eq $null){
+    Write-Output("No battery detected! Must be plugged into an external power source.") 
+  }
 }
