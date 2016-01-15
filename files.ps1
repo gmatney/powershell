4e6c4e2c7878cf190fa7b6788ec2819f620dc2c7
gmatney <gmatney@users.noreply.github.com>
2016-01-15 00:22:21 -0500

    For functions files and directories making them print relative path unless -full switch is added.  Also a depth option, which acts like a max depth for printing.

--- files.ps1
+++ files.ps1
@@ -1,22 +1,58 @@
 
 function md5($fileName){
-
-  $filePath = $(dir $fileName).FullName
-  $md5  = New-Object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
-  $hash = [System.BitConverter]::ToString($md5.ComputeHash([System.IO.File]::ReadAllBytes($filePath)))
-  Write-Output( $hash )
-
+	$filePath = $(dir $fileName).FullName
+	$md5  = New-Object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
+	$hash = [System.BitConverter]::ToString($md5.ComputeHash([System.IO.File]::ReadAllBytes($filePath)))
+	Write-Output( $hash )
 }
 
 function files{
-	#Get-ChildItem  -Recurse|?{!$_.PSIsContainer}|%{$_.FullName} 
-	gci -R|?{$_.PSIsContainer}|%{$_.FullName}
+	Param (
+		[Parameter(Mandatory=$false)][System.UInt32]$depth,
+		[Switch]$full
+	)
+	dirFilesHelper -dirOnly $false -depth $depth -full $full
 }
+
 
 function directories{
-	#Get-ChildItem  -Recurse|?{$_.PSIsContainer}|%{$_.FullName} 
-	gci -R|?{$_.PSIsContainer}|%{$_.FullName}
+	Param (
+		[Parameter(Mandatory=$false)][System.UInt32]$depth,
+		[Switch]$full
+	)
+	dirFilesHelper -dirOnly $true -depth $depth -full $full
 }
 
 
 
+function dirFilesHelper{
+	Param (
+		[Parameter(Mandatory=$true)][bool]$dirOnly,
+		[Parameter(Mandatory=$false)][System.UInt32]$depth,
+		[Parameter(Mandatory=$false)][bool]$full
+	)
+	#Get-ChildItem  -Recurse|?{$_.PSIsContainer}|%{$_.FullName} 
+	
+	$dirLength=0;
+	if(!$full){
+		$dirLength=((pwd).Path.Length+1)
+	}
+	
+	if($depth){
+		if($depth -lt 1){ 
+			if($dirOnly){gci . |?{ $_.PSIsContainer}|%{$_.FullName.subString($dirLength)}}
+			else        {gci . |?{!$_.PSIsContainer}|%{$_.FullName.subString($dirLength)}}
+		}
+		else{
+			$depth=$depth-1;
+			if($dirOnly){gci -R -depth $depth|?{ $_.PSIsContainer}|%{$_.FullName.subString($dirLength)}}
+			else        {gci -R -depth $depth|?{!$_.PSIsContainer}|%{$_.FullName.subString($dirLength)}}
+		}
+		
+	}
+	else{
+		gci -R |?{$_.PSIsContainer}|%{$_.FullName.subString($dirLength)}
+		if($dirOnly){gci -R |?{ $_.PSIsContainer}|%{$_.FullName.subString($dirLength)}}
+		else        {gci -R |?{!$_.PSIsContainer}|%{$_.FullName.subString($dirLength)}}
+	}
+}
