f8a69a57f7232719608950b9b402165efd1e69f5
gmatney <gmatney@users.noreply.github.com>
2016-01-19 20:59:19 -0500

    Adding loadAssembliesFromDirsAndAddTypeFromSource,
    Making it easier to Add-Type custom C# classes that will be using a lot
    of DLLs stuck in random folders and not in the GAC.

--- assemblyHelper.ps1
+++ assemblyHelper.ps1
@@ -20,6 +20,40 @@ function detect_assemblies2{
 		} catch{<# Ignore modules that don't contain assembly manifest #>}}
 }
 	
+
+#Leads from:   http://stackoverflow.com/questions/5475122/how-to-use-add-type-with-path-and-also-language-csharpversion3
+function loadAssembliesFromDirsAndAddTypeFromSource{
+	param(
+		$assemblyDirCollection,
+		$scriptPath,
+		$blackList             #Assemblies not to load
+	)
+	$addedAlready= New-Object System.Collections.ArrayList
+	$cp = new-object codedom.compiler.compilerparameters
+	foreach ($as in $assemblyDirCollection){
+		gci -R $as *.dll |%{ 
+			try{
+				($a = [Reflection.Assembly]::LoadFrom($_.FullName)  ) -and
+				(! ($addedAlready -contains  $a.GetName.Name)       ) -and
+				(! ($blackList    -contains  $a.GetName.Name)       ) -and
+				($cp.ReferencedAssemblies.Add($_.FullName) -or $true) -and
+				$addedAlready.Add(           $a.GetName.Name        )
+			}catch{<#Ignore DLL files without manifests and so on #>
+			}
+		}|Out-Null
+	}
+	Write-Output("Loaded "+$addedAlready)
+	# optionally turn on debugging support
+	if ($debugscript){
+		$cp.TreatWarningsAsErrors = $true
+		$cp.IncludeDebugInformation = $true
+		$cp.OutputAssembly = $env:temp + '\-' + [diagnostics.process]::getcurrentprocess().id + '.dll'
+	}
+
+	$script = [io.file]::readalltext($scriptPath)
+	add-type $script -lang csharp -compilerparam $cp
+
+}
 	
 
 Set-Alias -name ildasm  -value 'C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\ildasm.exe'  -description "IL Disassembler"
