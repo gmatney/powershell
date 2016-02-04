177d310020b5d7f14a712ded842bee8310d55f42
gmatney <garnett3m@gmail.com>
2016-02-03 21:51:42 -0500

    Adding placeholder file for API call to get info about books. Would still need to find a way to covert from more useful terms to ISIN or other library identifiers.

---
+++ books.ps1
@@ -0,0 +1,12 @@
+#Place to put scripts related to getting more info about books.
+
+
+if($false){
+	$w=Invoke-WebRequest 'https://openlibrary.org/api/books?bibkeys=ISBN:9780980200447&jscmd=data&format=json'
+
+
+
+}
+
+
+
