3e56360f9f809d751a20d98e0b2962254cd2b0f2
gmatney <garnett3m@gmail.com>
2016-03-22 23:09:33 -0400

    Adding file with starting stubs on generating a random org heirarchy for use with d3 trees

---
+++ DataGenerator/generate_random_org_heirarchy.ps1
@@ -0,0 +1,22 @@
+function generate_org_associations($maxDepth, $minSub, $maxSub){
+	
+}
+
+function __generate_org_association($maxDepth, $parentName, $minSub, $maxSub){
+	
+}
+
+
+# Load the random name files only after function called.  Clear afterwards
+#  Things to consider:   PersonId, FirstName, LastName, Level, CategoryTypeA, CategoryTypeB, Location, Gender 
+#  Mqy should think of generic descriptors types.  Example Level is a progression, but category Type might be like an enum
+function __generate_random_person(){
+	
+}
+
+
+# Find amusing lists to generate from 
+function  __generate_random_department(){
+
+
+}
