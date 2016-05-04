0618d541a6162f4aa8ed1bbbdc5e05f015a72ce2
gmatney <garnett3m@gmail.com>
2016-05-03 21:09:17 -0400

    Added functions to generate random popular names

--- DataGenerator/generate_random_org_heirarchy.ps1
+++ DataGenerator/generate_random_org_heirarchy.ps1
@@ -1,3 +1,31 @@
+$surnames         = Import-Csv -Delimiter "`t" -Path         Surnames.tsv
+$maleFirstNames   = Import-Csv -Delimiter "`t" -Path   MaleFirstNames.tsv
+$femaleFirstNames = Import-Csv -Delimiter "`t" -Path FemaleFirstNames.tsv
+
+function random_popular_surname(){
+    return $surnames[$(Get-Random -minimum 0 -maximum $surnames.Count)].Name
+}
+
+function random_popular_female_firstname(){
+    return $femaleFirstNames[$(Get-Random -minimum 0 -maximum $femaleFirstNames.Count)].Name
+}
+
+function random_popular_male_firstname(){
+    return $maleFirstNames[$(Get-Random -minimum 0 -maximum $maleFirstNames.Count)].Name
+}
+
+function random_popular_name(){
+    $randomName=""
+    if( $(Get-Random -minimum 0 -maximum 2) -eq 0 ){
+        $randomName = random_popular_female_firstname
+    }
+    else{
+        $randomName = random_popular_male_firstname
+    }
+    $randomName += " " + $(random_popular_surname)
+    Write-Output($randomName)
+}
+
 function generate_org_associations($maxDepth, $minSub, $maxSub){
 	
 }
@@ -17,6 +45,6 @@ function __generate_random_person(){
 
 # Find amusing lists to generate from 
 function  __generate_random_department(){
-
+	
 
 }
